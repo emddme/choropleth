6b6e2b2fdbed05e6ae034a9648cec50aeb92b2ab
Edward Derks <edward.derks@gmail.com>
2024-07-08 17:54:17 +0200

    use external i/o local data

--- index.js
+++ index.js
@@ -23,26 +23,30 @@ import drawLegend from "./functions/drawLegend.js";
 import drawNation from "./functions/drawNation.js";
 import drawStates from "./functions/drawStates.js";
 import drawTooltip from "./functions/drawTooltip.js";
-import fetchData from "./functions/fetchData.js";
-import getStatic from "./functions/getStatic.js";
+import getData from "./functions/getData.js";
 import highlight from "./functions/highlight.js";
 import insertTitleDescription from "./functions/insertTitleDescription.js";
 import removeTooltip from "./functions/removeTooltip.js";
 
-//get data
-const topoStatic = await getStatic(topoURL_static);
-const eduStatic = await getStatic(educationURL_static);
+//get static data
+// const topo = await getData(topoURL_static);
+// const edu = await getData(educationURL_static);
+
+//get external data
+
+const topo = await getData(topoURL);
+const edu = await getData(educationURL);
 
 //convert topology data to features collection
-const nation = convert(topoStatic, "nation");
-const states = convert(topoStatic, "states");
-const counties = convert(topoStatic, "counties").sort((a, b) => a.id - b.id);
+const nation = convert(topo, "nation");
+const states = convert(topo, "states");
+const counties = convert(topo, "counties").sort((a, b) => a.id - b.id);
 
 //define colorScale
-const scale = colorScale(eduStatic);
+const scale = colorScale(edu);
 
 //add education data to "counties" features collection, incl. scaled color and mean coordinate
-combine(counties, eduStatic, scale);
+combine(counties, edu, scale);
 
 //insert title and description
 insertTitleDescription("#app", title, description);
